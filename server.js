1a1211627625d9e7ddf056727162f91e5ee01e64
Sergey <elenergetica87@mail.ru>
2017-04-29 12:13:14 +0300

    Node server playground

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engie', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome on this page'
+  });
+});
+
+//
+// app.get('/', (req, res) => {
+//   // res.send('<h1>Hello Express!</h1>');
+//   res.send({
+//     name: "Sergey",
+//     likes: [
+//       'eat',
+//       'drinks'
+//     ]
+//   });
+// });
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: "It's error message",
+    description: "It's error message"
+
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server start on PORT 3000')
+});
